220ddb6f6177f7b6812c8c81198b93bbf912c91f
Gavrilo Mumovic <gajo357@gmail.com>
2020-04-14 20:18:40 +0200

    Using Form.List for competition

--- frontend/rashtan-auditstory-tsclient/src/components/Story/StoryCompetition.tsx
+++ frontend/rashtan-auditstory-tsclient/src/components/Story/StoryCompetition.tsx
@@ -1,39 +1,73 @@
 import React from "react";
-import { Form, Input } from "antd";
+import { Form, Input, Typography, Row, Col, InputNumber, Button } from "antd";
 import { CompanyCompetition } from "../../models/Company";
 import StoryPartForm, { StoryPartProps, FormItem } from "./StoryPartForm";
-import EditableTable, { ColumnInfo } from "../EditableTable";
+import styles from "./Story-styles";
+import { MinusCircleOutlined, PlusOutlined } from "@ant-design/icons";
 
 const StoryCompetition: React.FC<StoryPartProps<CompanyCompetition>> = ({
   data,
   dataChanged,
 }) => {
-  const columns: ColumnInfo[] = [
-    {
-      title: "Name",
-      key: "name",
-      type: "text",
-    },
-    {
-      title: "Market Cap",
-      key: "marketCap",
-      type: "number",
-    },
-    {
-      title: "Market Share (%)",
-      key: "marketShare",
-      type: "number",
-    },
-  ];
-
   return (
     <StoryPartForm title="Competition" data={data} dataChanged={dataChanged}>
-      <EditableTable
-        title="Competitors"
-        data={data.competitors}
-        setData={(c) => dataChanged({ ...data, competitors: c })}
-        columns={columns}
-      />
+      <Form.List name="competitors">
+        {(fields, { add, remove }) => (
+          <div>
+            <Typography.Text strong style={styles.revenueBlockTitle}>
+              Competitors
+            </Typography.Text>
+            {fields.map((field) => (
+              <Row key={field.name} style={styles.revenueRow}>
+                <Col>
+                  <Form.Item
+                    style={styles.revenueRowItem}
+                    name={[field.name, "name"]}
+                    label="Name"
+                    rules={[{ required: true }]}
+                  >
+                    <Input placeholder="Name" />
+                  </Form.Item>
+                </Col>
+                <Col>
+                  <Form.Item
+                    style={{ ...styles.revenueRowItem, marginLeft: 10 }}
+                    name={[field.name, "marketCap"]}
+                    label="Market Cap"
+                    rules={[{ required: true }]}
+                  >
+                    <InputNumber placeholder="Market Cap" />
+                  </Form.Item>
+                </Col>
+                <Col>
+                  <Form.Item
+                    style={{ ...styles.revenueRowItem, marginLeft: 10 }}
+                    name={[field.name, "marketShare"]}
+                    label="Market Share (%)"
+                    rules={[{ required: true }]}
+                  >
+                    <InputNumber placeholder="share %" />
+                  </Form.Item>
+                </Col>
+                <Col>
+                  <MinusCircleOutlined
+                    onClick={() => remove(field.name)}
+                    style={styles.checklistDelete}
+                  />
+                </Col>
+              </Row>
+            ))}
+            <Button
+              style={{ ...styles.addFlagButton, marginBottom: 10 }}
+              onClick={() => add()}
+              type="dashed"
+              icon={<PlusOutlined />}
+            >
+              Add competitor
+            </Button>
+          </div>
+        )}
+      </Form.List>
 
       <FormItem label="Industry growth" name="industryGrowth">
         <Input placeholder="Industry growth comment" />
