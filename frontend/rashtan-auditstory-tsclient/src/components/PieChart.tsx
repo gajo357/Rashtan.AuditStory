de00dc07e33f102afef9086caa8dc47c0a73992f
Gavrilo Mumovic <gajo357@gmail.com>
2020-04-22 22:44:01 +0200

    Added PieChart

---
+++ frontend/rashtan-auditstory-tsclient/src/components/PieChart.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Chart, Geom, Tooltip, Coord, Label } from "bizcharts";
+import { createColorSet } from "./BarChart";
+
+interface Props {
+  data: any[];
+
+  xField: string;
+  yField: string;
+}
+
+const PieChart: React.FC<Props> = ({ data, xField, yField, ...rest }) => {
+  const colorSet = createColorSet(data, xField);
+  return (
+    <Chart data={data} forceFit height={300} {...rest}>
+      <Coord type="theta" />
+      <Tooltip showTitle={false} />
+      <Geom
+        type="intervalStack"
+        position={yField}
+        color={[xField, (value) => colorSet[value]]}
+      >
+        <Label content={xField} />
+      </Geom>
+    </Chart>
+  );
+};
+
+export default PieChart;
