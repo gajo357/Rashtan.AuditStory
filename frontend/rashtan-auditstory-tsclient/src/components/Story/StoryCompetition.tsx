0bbda1fb4250fd454f5bf8885eea3aec1c8f0f5f
Gavrilo Mumovic <gajo357@gmail.com>
2020-04-22 20:50:45 +0200

    Market cap chart in the Competition

--- frontend/rashtan-auditstory-tsclient/src/components/Story/StoryCompetition.tsx
+++ frontend/rashtan-auditstory-tsclient/src/components/Story/StoryCompetition.tsx
@@ -3,6 +3,7 @@ import { Form, Input, InputNumber } from "antd";
 import StoryPartForm, { StoryPartProps } from "./StoryPartForm";
 import EditComment from "../SimpleEditors/EditComment";
 import EditableTable from "../EditableTable";
+import BarChart from "../BarChart";
 import { CompanyCompetition, UnitOfSize } from "../../models/Company";
 
 const StoryCompetition: React.FC<StoryPartProps<CompanyCompetition>> = ({
@@ -10,6 +11,12 @@ const StoryCompetition: React.FC<StoryPartProps<CompanyCompetition>> = ({
   onChange,
   currency,
 }) => {
+  const marketCapTitle = `Market Cap ${
+    currency ? ` (${UnitOfSize[currency.unit]} ${currency.currency})` : ""
+  }`;
+
+  const chartData = value.competitors.filter((c) => c && c.name && c.marketCap);
+
   return (
     <StoryPartForm title="Competition" value={value} onChange={onChange}>
       <EditableTable
@@ -21,11 +28,7 @@ const StoryCompetition: React.FC<StoryPartProps<CompanyCompetition>> = ({
             editor: <Input placeholder="Name" />,
           },
           {
-            title: `Market Cap ${
-              currency
-                ? ` (${UnitOfSize[currency.unit]} ${currency.currency})`
-                : ""
-            }}`,
+            title: marketCapTitle,
             fieldName: "marketCap",
             editor: <InputNumber placeholder="Market Cap" />,
           },
@@ -36,7 +39,15 @@ const StoryCompetition: React.FC<StoryPartProps<CompanyCompetition>> = ({
           },
         ]}
       />
-
+      {chartData.length > 0 && (
+        <BarChart
+          data={chartData}
+          xField="name"
+          yField="marketCap"
+          xTitle="Name"
+          yTitle={marketCapTitle}
+        />
+      )}
       <Form.Item label="Industry growth" name="industryGrowth">
         <Input placeholder="Industry growth comment" />
       </Form.Item>
