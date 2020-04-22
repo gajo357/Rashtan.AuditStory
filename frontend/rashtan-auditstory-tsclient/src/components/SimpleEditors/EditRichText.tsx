7ace380386855d59a55022ae811bbb2b35d6748f
Gavrilo Mumovic <gajo357@gmail.com>
2020-04-22 13:54:29 +0200

    Styling of the Rich Edit to match the dark mood

--- frontend/rashtan-auditstory-tsclient/src/components/SimpleEditors/EditRichText.tsx
+++ frontend/rashtan-auditstory-tsclient/src/components/SimpleEditors/EditRichText.tsx
@@ -9,7 +9,6 @@ interface Props {
 const EditRichText: React.FC<Props> = ({ value, onChange }) => {
   return (
     <ReactQuill
-      style={{ backgroundColor: "rgba(255, 255, 255, 0.6)" }}
       theme="snow"
       placeholder="Write whatever you want:)"
       value={value}
